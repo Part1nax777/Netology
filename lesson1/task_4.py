99ccfd54afcd7dd9914c70da3d6113152f4416da
part1nax777 <serewka-ru@yandex.ru>
2020-12-11 18:13:45 +0300

    add task_4

---
+++ lesson1/task_4.py
@@ -0,0 +1,15 @@
+print('Укажите ширину:')
+width = int(input())
+print('Укажите длину:')
+length = int(input())
+print('Укажите высоту:')
+height = int(input())
+
+if (width < 15) and (length < 15) and (height < 15):
+	print('Коробка №1')
+elif (15 < width < 50) or (15 < length < 50) or (15 < height < 50):
+	print('Коробка №2')
+elif length > 200:
+	print ('Упаковка для лыж')
+else:
+	print('Коробка №3')
