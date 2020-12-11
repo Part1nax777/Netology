dfd628da90aee2f0d682fac6432523662522229a
part1nax777 <serewka-ru@yandex.ru>
2020-12-11 15:59:55 +0300

    add task_1

---
+++ lesson1/task_1.py
@@ -0,0 +1,9 @@
+phrase_1 = 'hello'
+phrase_2 = 'hello world'
+
+if len(phrase_1) > len(phrase_2):
+	print('Фраза 1 длиннее фразы 2')
+elif len(phrase_1) < len(phrase_2):
+	print('Фраза 2 длиннее фразы 1')
+else:
+	print('Фразы равны')
